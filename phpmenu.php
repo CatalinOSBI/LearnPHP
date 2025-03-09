f4c54b83662d2beaaf79e3d63a784cc41ca942f7
Catalin Girjoaba <catalin.gameing@gmail.com>
2025-03-09 19:20:04 +0200

    more practice

--- phpmenu.php
+++ phpmenu.php
@@ -26,7 +26,7 @@
     }
     ?>
 
-        <!-- Loop Counter -->
+    <!-- Loop Counter -->
 
     <form action="phpmenu.php" method="post">
         <label for="maxCount">Loop Counter:</label>
@@ -40,11 +40,118 @@
             $maxCount = $_POST['maxCount'];
 
             for ($i = 0; $i <= $maxCount; $i++) {
-                echo $i .'|';
+                echo $i . '|';
             }
         }
     }
     ?>
+
+    <!-- Basic Function -->
+
+    <form action="phpmenu.php" method="post">
+        <label for="firstValue">First Value:</label>
+        <input type="number" name="firstValue" id="firstValue">
+        <label for="secondValue">Second Value:</label>
+        <input type="number" name="secondValue" id="SecondValue">
+        <button type="submit" name="addSubmit">Add</button>
+    </form>
+
+    <?php
+    function addFunction($a, $b)
+    {
+        $result = $a + $b;
+        echo $result;
+    }
+
+    if (isset($_POST['addSubmit'])) {
+        if (!empty($_POST['firstValue'] || !empty($_POST['secondValue']))) {
+            addFunction($_POST['firstValue'], $_POST['secondValue']);
+        }
+    }
+    ?>
+
+            <!-- Basic Sanitization -->
+
+    <form action="phpmenu.php" method="post">
+        <label for="sanExample">(Sanitised) Print:</label>
+        <input name="sanExample" type="text" placeholder='<script>alert("test")</script>' id="sanExample">
+        <button type="submit" name="sanSubmit">Echo (Sanitised)</button>
+    </form>
+
+    <?php
+
+    if (isset($_POST["sanSubmit"])) {
+        $sanText = $_POST['sanExample'];
+        $sanitisedText = htmlspecialchars($sanText);
+        $sanitisedText2 = strip_tags($sanText);
+
+        echo '(1) '. $sanitisedText . '<br>';
+        echo '(2) '. $sanitisedText2;
+    }
+    ?>
+
+        <!-- Basic Validation -->
+
+    <form action="phpmenu.php" method="post">
+        <label for="valExample">(Validated) Print:</label>
+        <input name="valExample" type="text" placeholder="example@gmail.com" id="valExample">
+        <button type="submit" name="valSubmit">Echo (Validated)</button>
+    </form>
+
+    <?php
+        if (isset($_POST['valSubmit'])) {
+            if (!empty($_POST['valExample'])) {
+                if (filter_var($_POST['valExample'],FILTER_VALIDATE_EMAIL)) {
+                    $result = $_POST['valExample'];
+                    echo $result . ' is a valid email';
+                } else {   
+                    echo 'email not valid';
+                }
+            }
+        } 
+
+    ?>
+
+            <!-- Basic Class -->
+
+    <form action="phpmenu.php" method="post">
+        <label>Dog Class</label>
+        <button type="submit" name="classSubmit">Echo Class</button>
+    </form>
+
+    <?php
+    class Dog
+    {
+        private $breed;
+        private $name;
+        private $size;
+        private $weight;
+
+        public function __construct($breed, $name, $size, $weight) {
+            $this->breed = $breed;
+            $this->name = $name;
+            $this->size = $size;
+            $this->weight = $weight;
+        }
+
+        public function echoDog()
+        {
+            return 'Breed| '. $this->breed . '<br>'.
+                   'Name| '. $this->name . '<br>'.
+                   'Size| '. $this->size . '<br>'.
+                   'Weight| '. $this->weight;
+        }
+    } 
+
+    $rotweiller = new Dog('Rotweiller', 'Spike', 'Large', '50kg');
+
+    if (isset($_POST['classSubmit'])) {
+        echo $rotweiller->echoDog();
+    }
+
+    ?>
+
+    
 
 </body>
 
